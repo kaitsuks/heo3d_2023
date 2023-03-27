7493951c954733fa172e4f2e3981b7b6ef940eb1
Kai Kurki-Suonio <37402060+kaitsuks@users.noreply.github.com>
2023-03-27 21:06:49 +0300

    Wander
    No-force version added to Update()

--- Assets/Wander3DB.cs
+++ Assets/Wander3DB.cs
@@ -3,27 +3,26 @@ using System.Collections;
 using System.Collections.Specialized;
 
 //This script has been suggested by Bryan Livingston (@BryanLivingston). Thanks Bryan!
+//Modified to 3D from Unity Playground 2D original by Kaitsu
+//Force and no-force versions
+//If you want to use force version, comment out Update() and in FixedUpdate() /Kaitsu
 
 [AddComponentMenu("Kaitsu/Movement/Wander3DB")]
 [RequireComponent(typeof(Rigidbody))]
 public class Wander3DB : Physics3DObject
 {
 	[Header("Movement")]
-	public float speed = 1f;
+	public float speed = 5f;
 	public float directionChangeInterval = 3f;
 	public bool keepNearStartingPoint = true;
 
 	[Header("Orientation")]
 	public bool orientToDirection = true;
 	// The direction that the GameObject will be oriented to
-	public Enums.Directions lookAxis = Enums.Directions.Up;
-
+	public Enums.Directions lookAxis = Enums.Directions.Right;
 
 	private Vector3 direction;
 	private Vector3 startingPoint;
-
-	
-
 
 	// Start is called at the beginning of the game
 	private void Start()
@@ -40,15 +39,13 @@ public class Wander3DB : Physics3DObject
 		StartCoroutine(ChangeDirection());
 	}
 
-
-
-
 	// Calculates a new direction
 	private IEnumerator ChangeDirection()
 	{
 		while (true)
 		{
-			direction = Random.insideUnitSphere; //change the direction the player is going
+			direction = Random.insideUnitSphere * 5; //change the direction the player is going
+			direction.y = 0;
 
 			// if we need to keep near the starting point...
 			if (keepNearStartingPoint)
@@ -62,26 +59,37 @@ public class Wander3DB : Physics3DObject
 				}
 			}
 
-
 			//if the object has to look in the direction of movement
 			if (orientToDirection)
 			{
 				Utils3D.SetAxisTowards(lookAxis, transform, direction);
 			}
 
-
 			// this will make Unity wait for some time before continuing the execution of the code
 			yield return new WaitForSeconds(directionChangeInterval);
 		}
 	}
 
+    private void Update()
+    {
+		//No physics force version
+
+		//You've already calculated and stored the movement direction, so all you need to do is transform
+		//	that into a quaternion and assign it in your Update function and you're done;
+		//alien.transform.rotation = Quaternion.LookRotation(movementDirection);
+		//If you want the transition to be smoother, you can interpolate the rotation as well;
+		//alien.transform.rotation = Quaternion.Slerp(alien.transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime * 40f);
+
+		transform.rotation = Quaternion.LookRotation(direction);
+
+		// Move the object forward along its z axis, speed units/second.
+		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
 
 
-	// FixedUpdate is called every frame when the physics are calculated
-	private void FixedUpdate()
-	{
-		direction.y = 0;
-		rigidbody.AddForce(-direction * speed);
-		//rigidbody.AddForce(Vector3.up * speed);
+    // FixedUpdate is called every frame when the physics are calculated
+    private void FixedUpdate()
+	{	//Physics version with force
+		//rigidbody.AddForce(direction * speed);
 	}
 }
